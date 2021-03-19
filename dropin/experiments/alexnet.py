d69a44e657a1f16ff84d6f4d31b98b3f66b40622
Ali Asgari <altostratous@gmail.com>
2021-03-19 13:21:57 -0700

    Fix top one accuracy

--- dropin/experiments/alexnet.py
+++ dropin/experiments/alexnet.py
@@ -74,7 +74,7 @@ class AlexNet(ExperimentBase):
             'y_true': np.argmax(y_true, axis=1),
             'y_pred': np.argsort(y_pred, axis=1).T[-5:].T
         }
-        logger.info('Evaluation Accuracy: {}'.format(np.average(evaluation['acc'])))
+        logger.info('Evaluation Accuracy: {}'.format(np.average(np.argsort(y_pred, axis=1).T[-1:].T == y_pred)))
         return evaluation
 
     def get_faulty_model(self, config, name=None):
